b248b21427d64da04b53753becaac274193bcfde
Tazodiac <tasmyn.clark@gmail.com>
2020-03-03 11:55:17 -0500

    Add more code to repeat prompt if no input is added and change options colour to match the colour inputted.

--- js/main.js
+++ js/main.js
@@ -6,21 +6,36 @@ var pickStory;
 
 alert('Let’s make a story together!');
 
-pickColour = prompt('A colour (orange, indigo, ect.)');
-pickCreature = prompt('A creature (dragon, moth, etc.)');
-pickAdj = prompt('An adjective (beautiful, super, etc.)');
-pickPast = prompt('A past tense verb (ran, burped, etc.)');
-pickStory = prompt('Which story would you like? \n a) Snacking \n b) Escaping \n c) Winning \n Enter the letter below:');
+while (!pickColour) {
+  pickColour = prompt('A colour (orange, indigo, ect.)');
+}
+
+while (!pickCreature) {
+  pickCreature = prompt('A creature (dragon, moth, etc.)');
+}
+
+while (!pickAdj) {
+  pickAdj = prompt('An adjective (beautiful, super, etc.)');
+}
+
+while (!pickPast) {
+  pickPast = prompt('A past tense verb (ran, burped, etc.)');
+}
+
+while (!['a', 'b', 'c'].includes(pickStory)) {
+  pickStory = prompt('Which story would you like? \n a) Snacking \n b) Escaping \n c) Winning \n Enter the letter below:');
+}
+
+document.documentElement.style.setProperty('--colour', pickColour);
 
 if (pickStory == 'a') {
-  document.write(`After snacking on ${pickAdj} treats, the ${pickColour} bellied ${pickCreature} ${pickPast} for hours.`);
+  document.write(`After snacking on <span>${pickAdj}</span> treats, the <span>${pickColour}</span> bellied <span>${pickCreature}</span> <span>${pickPast}</span> for hours.`);
 }
 
 if (pickStory == 'b') {
-  document.write(`Amelia ${pickPast} through the ${pickAdj} ${pickColour} nebula escaping the space ${pickCreature}.`);
+  document.write(`Amelia <span>${pickPast}</span> through the <span>${pickAdj}</span> <span>${pickColour}</span> nebula escaping the space <span>${pickCreature}</span>.`);
 }
 
 if (pickStory == 'c') {
-  document.write(`Jackson chose his ${pickAdj}, ${pickColour} ${pickCreature} card and ${pickAdj} it to the table knowing he won.`);
-
+  document.write(`Jackson chose his <span>${pickAdj}</span>, <span>${pickColour}</span> <span>${pickCreature}</span> card and <span>${pickAdj}</span> it to the table knowing he won.`);
 }
