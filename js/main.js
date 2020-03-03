0ab546d7cb6e3b80410ea0aa41f319c7dec8cc56
Tazodiac <tasmyn.clark@gmail.com>
2020-03-03 11:16:57 -0500

    Change the variables to the top

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
+var pickColour;
+var pickCreature;
+var pickAdj;
+var pickPast;
+var pickStory;
+
 alert('Let’s make a story together!');
 
-var pickColour = prompt('A colour (orange, indigo, ect.)');
-var pickCreature = prompt('A creature (dragon, moth, etc.)');
-var pickAdj = prompt('An adjective (beautiful, super, etc.)');
-var pickPast = prompt('A past tense verb (ran, burped, etc.)');
-var pickStory = prompt('Which story would you like? \n a) Snacking \n b) Escaping \n c) Winning \n Enter the letter below:');
+pickColour = prompt('A colour (orange, indigo, ect.)');
+pickCreature = prompt('A creature (dragon, moth, etc.)');
+pickAdj = prompt('An adjective (beautiful, super, etc.)');
+pickPast = prompt('A past tense verb (ran, burped, etc.)');
+pickStory = prompt('Which story would you like? \n a) Snacking \n b) Escaping \n c) Winning \n Enter the letter below:');
 
 if (pickStory == 'a') {
   document.write(`After snacking on ${pickAdj} treats, the ${pickColour} bellied ${pickCreature} ${pickPast} for hours.`);
